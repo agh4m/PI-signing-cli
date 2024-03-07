5b4cff06345f0394db71b80c7c2bcd876a0e1354
Tiago Figueiredo <tiagofigueiredo2003@gmail.com>
2024-03-07 10:15:44 +0000

    chore: include cxx.h

--- sig_lib/library.cpp
+++ sig_lib/library.cpp
@@ -1,9 +1,9 @@
-#include "library.h"
 
 #include <iostream>
+#include "rust/cxx.h"
 
 int sig_doc(rust::Str path) {
-    // TODO: sign documents with auth.gov
-    std::cout << "Signing document: " << path << std::endl;
-    return 0;
+  // TODO: sign documents with auth.gov
+  std::cout << "Signing document: " << path << std::endl;
+  return 0;
 }

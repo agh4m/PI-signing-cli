bd26c2dccbdc3831d1dc6674d33c99e6c641b6e5
agh4m <tiagofigueiredo2003@gmail.com>
2024-04-27 14:04:59 +0100

    fix: replace buttons with a tags to fix a black screen between each page

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -5,20 +5,32 @@
 </script>
 
 <div class="absolute left-0 flex h-screen w-52 flex-col gap-4 bg-zinc-900 p-5">
-	<Button class="bg-zinc-800 font-semibold shadow shadow-zinc-950" on:click={() => {window.location.href = "/dashboard/"}}>Upload Documents</Button>
-	<Button class="bg-zinc-800 font-semibold shadow shadow-zinc-950" on:click={() => {window.location.href = "/dashboard/download"}}>Download Documents</Button>
-	<Button class="bg-zinc-800 font-semibold shadow shadow-zinc-950" on:click={() => {window.location.href = "/dashboard/verify"}}>Verify Authenticity</Button>
+	<a href="/dashboard" data-sveltekit-preload-data="hover" class="w-44">
+		<Button class="w-full bg-zinc-800 font-semibold shadow shadow-zinc-950">
+			Upload Documents
+		</Button>
+	</a>
+	<a href="/dashboard/download" data-sveltekit-preload-data="hover" class="w-44">
+		<Button class="w-full bg-zinc-800 font-semibold shadow shadow-zinc-950">
+			Download Documents
+		</Button>
+	</a>
+	<a href="/dashboard/verify" data-sveltekit-preload-data="hover" class="w-44">
+		<Button class="bg-zinc-800 font-semibold shadow shadow-zinc-950 w-full">
+            Verify Authenticity
+        </Button>
+	</a>
 	<div class="flex-grow"></div>
 	<DropdownMenu.Root>
 		<DropdownMenu.Trigger class="flex items-center gap-1 text-zinc-300">
-			<UserCircleSolid class="h-6"/>
+			<UserCircleSolid class="h-6" />
 			Account
 		</DropdownMenu.Trigger>
-		<DropdownMenu.Content class="bg-black text-white border-black w-48">
+		<DropdownMenu.Content class="w-48 border-black bg-black text-white">
 			<DropdownMenu.Group>
-                <DropdownMenu.Item>My files</DropdownMenu.Item>
-				<DropdownMenu.Separator class="w-[90%] mx-auto text-zinc-300"/>
-                <DropdownMenu.Item>Log out</DropdownMenu.Item>
+				<DropdownMenu.Item>My files</DropdownMenu.Item>
+				<DropdownMenu.Separator class="mx-auto w-[90%] text-zinc-300" />
+				<DropdownMenu.Item>Log out</DropdownMenu.Item>
 			</DropdownMenu.Group>
 		</DropdownMenu.Content>
 	</DropdownMenu.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Button from '$lib/components/ui/button/button.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
-	let value = 'Password';
+	let value = '';
 </script>
 
 <div
@@ -9,12 +9,9 @@
 >
 	<h1 class="text-4xl font-bold text-white">Sign in to DiSA</h1>
 	<p class="mb-8 text-white">Start uploading your files today.</p>
-	<Input class="w-80 text-white" bind:value />
-	<Input class="w-80 text-white" bind:value type="password" />
-	<Button
-		class="shadown-md h-10 w-80 bg-zinc-700 font-semibold shadow-zinc-950"
-		on:click={() => (window.location.href = '/dashboard')}
-	>
-		Login
-	</Button>
+	<Input class="w-80 text-white" bind:value placeholder="Email" type="text" />
+	<Input class="w-80 text-white" bind:value type="password" placeholder="Password" />
+	<a href="/dashboard" data-sveltekit-preload-data="hover">
+		<Button class="shadown-md h-10 w-80 bg-zinc-700 font-semibold shadow-zinc-950">Login</Button>
+	</a>
 </div>

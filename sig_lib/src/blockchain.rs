afd93fca8ff88cbc19a240d246597a02673ba671
agh4m <tiagofigueiredo2003@gmail.com>
2024-04-28 18:26:48 +0100

    chore: add web3 to deps, move private key to func param

--- sig_lib/src/blockchain.rs
+++ sig_lib/src/blockchain.rs
@@ -1,25 +1,28 @@
-use web3::contract::{Contract, Options};
-use web3::types::{Address, Transaction, U256};
-use web3::transports::{Http};
-use dotenv_codegen::dotenv;
+use std::error::Error;
+use web3::contract::Contract;
+use web3::transports::Http;
+use web3::types::Transaction;
+use web3::{ethabi, Web3};
 
-
-pub async fn save_certificate(sig_hash: &str) -> Result<String, Box<dyn std::error::Error>> {
+pub async fn save_certificate(sig_hash: &str, private_key: &str) -> Result<String, Box<dyn Error>> {
     let node_url = "https://rpc-mumbai.maticvigil.com";
     let contract_address = "0x9e5c6b3d16411736c068026fc212e0b413dce243";
-    let private_key = dotenv!("PRIVATE_KEY");
-    
+
     let transport = Http::new(&node_url)?;
-    let web3 = web3::Web3::new(transport);
-    let wallet = web3::ethabi::Account::from_key(&private_key)?;
+    let web3 = Web3::new(transport);
+    let wallet = ethabi::Account::from_key(&private_key)?;
+
     let contract = Contract::new(&web3, *contract_address);
 
-    let estimated_gas = contract.estimate_gas("storeHash", vec![sig_hash.as_bytes().to_vec()]).await?;
-    let tx = Transaction::new()
-        .gas_price(estimated_gas);
+    let estimated_gas = contract
+        .estimate_gas("storeHash", vec![sig_hash.as_bytes().to_vec()])
+        .await?;
+    let tx = Transaction::new().gas_price(estimated_gas);
 
-    let call=contract.call("storeHash", vec![sig_hash.as_bytes().to_vec()]);
+    let call = contract.call("storeHash", vec![sig_hash.as_bytes().to_vec()]);
     let signed_tx = wallet.sign_transaction(tx, &call).await?;
     let tx_hash = web3.eth().send_transaction(signed_tx).await?;
-    Ok(tx_hash)
+
+    return Ok(tx_hash.to_string());
 }
+

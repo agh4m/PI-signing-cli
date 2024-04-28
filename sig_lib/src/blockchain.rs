9a61b8fdee478bcd9d8fdc74710af4b5ee6d536b
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-04-28 18:01:05 +0100

    remade according to suggestions

--- sig_lib/src/blockchain.rs
+++ sig_lib/src/blockchain.rs
@@ -1,13 +1,14 @@
 use web3::contract::{Contract, Options};
 use web3::types::{Address, Transaction, U256};
 use web3::transports::{Http};
-use std::env;
+use dotenv_codegen::dotenv;
 
-let node_url = "https://rpc-mumbai.maticvigil.com";
-let contract_address = "0x9e5c6b3d16411736c068026fc212e0b413dce243";
-let private_key = env::var("PRIVATE_KEY").unwrap_or_else(|| "default_value".to_string());
 
 pub async fn save_certificate(sig_hash: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let node_url = "https://rpc-mumbai.maticvigil.com";
+    let contract_address = "0x9e5c6b3d16411736c068026fc212e0b413dce243";
+    let private_key = dotenv!("PRIVATE_KEY");
+    
     let transport = Http::new(&node_url)?;
     let web3 = web3::Web3::new(transport);
     let wallet = web3::ethabi::Account::from_key(&private_key)?;

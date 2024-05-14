cd4b193b35715c569daa5162fb8a4881f124ea93
Tiago Figueiredo <tiagofigueiredo2003@gmail.com>
2024-05-14 16:26:43 +0100

    fix: cxx induced memory leak and excessive cpu usage

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,17 @@ export default defineConfig({
     plugins: [sveltekit()],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
-    }
+    },
+    server: {
+        port: 5173,
+        strictPort: true,
+        watch: {
+            ignored: [
+                '**/src-tauri/**/*',
+                '**/sig_lib/**/*',
+                '**/cli/**/*'
+            ],
+        }
+    },
+
 });
